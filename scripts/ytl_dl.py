3e02f478258d3b342bb966ff821a6c20c5132904
Jiho Noh <jnoh3@kennesaw.edu>
2024-09-13 18:45:02 -0400

    ytl update

--- scripts/ytl_dl.py
+++ scripts/ytl_dl.py
@@ -6,126 +6,139 @@ import code
 import os
 from dotenv import load_dotenv
 import argparse
-from tqdm import tqdm
 import json
 from itertools import islice
+from tqdm import tqdm
+import time
 
-import gspread
+
+# import gspread
 import googleapiclient
 import googleapiclient.discovery
 
+# https://github.com/jdepoix/youtube-transcript-api
 from youtube_transcript_api import YouTubeTranscriptApi as YTTA
 from youtube_transcript_api.formatters import JSONFormatter
 import youtube_comment_downloader as ytc
 
-
-def read_playlists(
-    client: gspread.Client,
-    sh_id: str,
-    sheet_name: str = "Sheet1",
-):
-    sh = client.open_by_key(sh_id)  # open the spreadsheet
-    # sh = client.open_by_url(
-    #     "https://docs.google.com/spreadsheets/d/1QAVrbPnitRay5iqdkrJ8Mk1jM1OUktPitVkXRi5tERk/edit#gid=0"
-    # )
-    worksheet = sh.worksheet(sheet_name)  # open the worksheet
-    # columns = ['playlistname', 'number-of_vds', 'link', 'listID']
-    playlists = worksheet.get_all_values()[1:]  # get all values except the header
-
-    return playlists
+SUBJ = {
+    "Biology": "bio",
+    "Chemistry": "chem",
+    "Mathematics": "math",
+    "Physics": "phy",
+    "Computer Science": "cs",
+}
+VID_STATS = {k: [] for k in SUBJ.keys()}
 
 
-def download_trans_comm(args, gsc, gac, ytc):
-    playlists = read_playlists(gsc, args.sheet_id, args.sheet_name)
-    vid_metadata = []
+def download_trans_comm(args, gac, ytc):
+    # Read the playlist file in JSON format
+    with open(args.pl_file, "r") as f:
+        playlists = json.load(f)
+
+    # read list of videos per playlist using the youtube api
+    vidlist = []
     for pl in tqdm(playlists):
         # playlistitems doc  https://tinyurl.com/z5ebnpb9
         nextpage = "firstpage"
         while nextpage:
             req = gac.playlistItems().list(
                 part="snippet",
-                playlistId=pl[3],
+                playlistId=pl["listID"],
                 maxResults=50,
                 pageToken=nextpage if nextpage != "firstpage" else None,
             )
             resp = req.execute()
+
             nextpage = resp.get("nextPageToken")
             for item in resp["items"]:
                 entry = {
                     "pid": item["snippet"]["playlistId"],
                     "vid": item["snippet"]["resourceId"]["videoId"],
                     "title": item["snippet"]["title"],
+                    "subject": pl["subject"],
                 }
-                vid_metadata.append(entry)
-    print(f"{len(vid_metadata)} videos found in the {len(playlists)} playlists")
+                vidlist.append(entry)
+    print(f"{len(vidlist)} videos found in the {len(playlists)} playlists")
 
-    # create a dictionary to store the transcripts and comments; .data/transcripts/ and .data/comments/
+    # create a dictionary to store the transcripts and comments;
+    #    .data/transcripts/ and .data/comments/
     os.makedirs(f"{args.output_dir}/transcripts", exist_ok=True)
     os.makedirs(f"{args.output_dir}/comments", exist_ok=True)
 
-    # JSON formatter
+    # YTTA JSON formatter
     formatter = JSONFormatter()
-    # download transcripts and comments
-    for vid in vid_metadata:
+    # get stats, download transcripts and comments
+    for vid in vidlist:
+        # get stats
+        stats = gac.videos().list(part="statistics", id=vid["vid"]).execute()
+        try:
+            VID_STATS[vid["subject"]].append(
+                [
+                    stats["items"][0]["statistics"][k]
+                    for k in ["viewCount", "likeCount", "commentCount"]
+                ]
+            )
+        except:
+            pass
+
         # if file exists, skip
-        skip = False
-        if os.path.exists(f"{args.output_dir}/transcripts/{vid['vid']}.json"):
+        tf = "{}/transcripts/{}-{}-trans.json" "".format(
+            args.output_dir, vid["vid"], SUBJ[vid["subject"]]
+        )
+        cf = "{}/comments/{}-{}-cmmt.json" "".format(
+            args.output_dir, vid["vid"], SUBJ[vid["subject"]]
+        )
+
+        if os.path.exists(tf):
             print(f"Skipping {vid['vid']}")
-            skip = True
-        if not skip:
+        else:
             # download transcript
             print(f"Downloading transcript for {vid['vid']}")
             try:
                 transcript = YTTA.get_transcript(vid["vid"])
             except:
-                print(f"Transcripts disabled for {vid['vid']}")
+                print(f"Failed to download transcript for {vid['vid']}")
             else:  # if no exception
-                json_formatted = formatter.format_transcript(transcript)
                 # save transcript
-                with open(
-                    f"{args.output_dir}/transcripts/{vid['vid']}.json",
-                    "w",
-                    encoding="utf-8",
-                ) as f:
-                    f.write(json_formatted)
+                with open(tf, "w", encoding="utf-8") as f:
+                    f.write(formatter.format_transcript(transcript))
 
         # download comments
-        skip = False
-        if os.path.exists(f"{args.output_dir}/comments/{vid['vid']}.json"):
+        if os.path.exists(cf):
             print(f"Skipping comments for {vid['vid']}")
-            skip = True
-        if not skip:
+        else:
             print(f"Downloading comments for {vid['vid']}")
             comments = []
-            for c in islice(ytc.get_comments(vid["vid"]), 1000):
-                entry = {
-                    "vid": vid["vid"],
-                    "cid": c["cid"],
-                    "comment": c["text"].replace("\n", " "),
-                    "votes": c["votes"],
-                    "replies": c["replies"],
-                    "reply": c["reply"],
-                }
-                comments.append(entry)
+            try:
+                resp = ytc.get_comments(vid["vid"])
+            except:
+                print(f"Failed to download comments for {vid['vid']}")
+            else:
+                for c in islice(resp, 1000):
+                    entry = {
+                        "vid": vid["vid"],
+                        "cid": c["cid"],
+                        "comment": c["text"].replace("\n", " "),
+                        "votes": c["votes"],
+                        "replies": c["replies"],
+                        "reply": c["reply"],
+                    }
+                    comments.append(entry)
+                # save comments
+                with open(cf, "w", encoding="utf-8") as f:
+                    json.dump(comments, f)
 
-            # save comments
-            with open(
-                f"{args.output_dir}/comments/{vid['vid']}.json",
-                "w",
-                encoding="utf-8",
-            ) as f:
-                json.dump(comments, f)
+    # save stats with unix timestamp
+    statfile = "{}/stats-{}.json".format(args.output_dir, int(time.time()))
+    with open(statfile, "w") as f:
+        json.dump(VID_STATS, f)
 
 
 if __name__ == "__main__":
     # args
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument(
-        "--sheet_id", type=str, required=True, help="Google spreadsheet id"
-    )
-    arg_parser.add_argument(
-        "--sheet_name", type=str, default="Sheet1", help="Google spreadsheet name"
-    )
+    arg_parser.add_argument("--pl_file", type=str, required=True, help="Playlist file")
     arg_parser.add_argument(
         "--output_dir", type=str, default="data", help="Output directory"
     )
@@ -134,11 +147,10 @@ if __name__ == "__main__":
     # load env variables
     load_dotenv()
 
-    # gspread client
-    gs_client = gspread.service_account()
+    # gs_client = gspread.service_account()
+    # YouTube API client
     ga_client = googleapiclient.discovery.build(
         "youtube", "v3", developerKey=os.environ["YT_API_KEY"]
     )
     ytc_client = ytc.YoutubeCommentDownloader()
-
-    download_trans_comm(args, gs_client, ga_client, ytc_client)
+    download_trans_comm(args, ga_client, ytc_client)
